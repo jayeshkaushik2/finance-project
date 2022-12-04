e3c6679d188d54b6c72cd5fb360d77f04005f440
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-12-04 20:17:42 +0530

    added common, accounts and serializer files

--- incomes/models.py
+++ incomes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Income(models.Model):
+    source = models.CharField(max_length=256, null=True, blank=True)
+    salary = models.PositiveBigIntegerField(null=True, blank=True, default=0)
