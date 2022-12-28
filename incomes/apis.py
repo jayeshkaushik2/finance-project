db24386a892cdaaf8e7badabb74d503e8790ee07
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-12-28 20:34:23 +0530

    feature: added api for summary details.

--- incomes/apis.py
+++ incomes/apis.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 # local imports
 from .models import Income
 from .serializers import IncomeSz
+from django.db.models import Sum, F
 
 
 class IncomeApi(viewsets.ModelViewSet):
@@ -23,8 +24,33 @@ class IncomeApi(viewsets.ModelViewSet):
 @decorators.permission_classes([permissions.IsAuthenticated])
 def incomeReportApi(request, weeks):
     user = request.user
-    current_date = timezone.now().date()
     previous_date = timezone.now().date() - timedelta(weeks=int(weeks))
     incomes = Income.objects.filter(user=user, created_at__date__gte=previous_date)
     sz = IncomeSz(instance=incomes, many=True)
     return Response(sz.data)
+
+
+from spendings.models import Spending
+
+
+@decorators.api_view(["GET"])
+@decorators.permission_classes([permissions.IsAuthenticated])
+def SummaryReportApi(request):
+    user = request.user
+    current_month = timezone.now().date().strftime("%m")
+
+    current_month_incomes = Income.objects.filter(
+        user=user, created_at__date__month=current_month
+    ).aggregate(Sum("salary"))
+
+    current_month_spendings = Spending.objects.filter(
+        user=user, created_at__date__month=current_month
+    ).aggregate(Sum("spent_money"))
+
+    data = dict(
+        total_income=current_month_incomes["salary__sum"],
+        total_spending=current_month_spendings["spent_money__sum"],
+        total_saving=current_month_incomes["salary__sum"]
+        - current_month_spendings["spent_money__sum"],
+    )
+    return Response(data)
--- incomes/routes.py
+++ incomes/routes.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .apis import IncomeApi, incomeReportApi
+from .apis import IncomeApi, incomeReportApi, SummaryReportApi
 
 router = DefaultRouter()
 router.register(prefix="income", viewset=IncomeApi, basename="income")
 
 urlpatterns = [
-    path("income-report/<int:weeks>/", incomeReportApi, name="income_report")
+    path("income-report/<int:weeks>/", incomeReportApi, name="income_report"),
+    path("summary-report/", SummaryReportApi, name="summary_report"),
 ]
 urlpatterns += router.urls
