553a34eeef1630fb7daabe0a7894bd4ba3759d6a
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2023-01-08 16:19:57 +0530

    feature: fixed sz of user details and spending.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -53,6 +53,13 @@ class RegisterUserValidationSz(serializers.Serializer):
 
 
 class UserDetailSz(serializers.ModelSerializer):
+    profile_image = serializers.ImageField(
+        max_length=None, use_url=True, allow_null=True, required=False
+    )
+    banner_image = serializers.ImageField(
+        max_length=None, use_url=True, allow_null=True, required=False
+    )
+
     class Meta:
         model = User
         fields = (
--- spendings/serializers.py
+++ spendings/serializers.py
@@ -9,13 +9,13 @@ class SpendingSz(serializers.ModelSerializer):
         model = Spending
         fields = (
             "id",
-            "user",
             "spent_on",
             "spent_money",
             "month",
             "created_at",
             "updated_at",
         )
+
     def get_month(self, obj):
         if obj.created_at is not None:
             return obj.created_at.strftime("%B")
